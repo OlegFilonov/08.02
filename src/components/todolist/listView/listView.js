6f45602f19870ffa94915ef97f70c90b11a0c783
Oleg Filonov <folegdonetsk@yandex.ru>
2019-10-28 23:01:03 +0300

    finally

--- src/components/todolist/listView/listView.js
+++ src/components/todolist/listView/listView.js
@@ -13,7 +13,6 @@ export default function ListView(props) {
                         <button onClick={() => onDelete(item)}>x</button>
                     </li>)
             })}
-
         </ul>
     )
 }
